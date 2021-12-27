69ff026094f8035e60cba10b1332db1374159783
Stef Vanaken <stef.vanaken1@student.kuleuven.be>
2021-12-27 19:25:46 +0100

    refactoring

--- encode.py
+++ encode.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python3
-from dotenv import load_dotenv
-load_dotenv()
+from config import *
 
 import os
 import time
-import keyboard
 
-inp = os.environ.get('inp')
-output = os.environ.get('output')
-remote = os.environ.get('remote')
-adress = os.environ.get('adress')
-preset = os.environ.get('preset')
-if not os.path.isdir(inp+ "/in"):
-    os.mkdir(inp+"/in")
-if not os.path.isdir(output):
-    os.mkdir(inp+"/out")
+if not os.path.isdir(input_dir+ "/in"):
+    os.mkdir(input_dir+"/in")
+if not os.path.isdir(output_dir):
+    os.mkdir(input_dir+"/out")
 
 files = []
 processing = []
 while 1:
-    for file in os.listdir(inp + "/in"):
+    for file in os.listdir(input_dir + "/in"):
             if file.endswith(".mp4") and file not in files and file not in processing:
                     files.append(file)
     if len(files)>0 and len(processing)<1:
         first = files.pop()
         processing.append(first)
-        st = "HandBrakeCLI -i {0}/in/{1} -o {2}/{1} --preset-import-file {0}/{4}.json -Z '{4}'".format(inp,first,output, preset)
+        st = f'HandBrakeCLI -i {input_dir}/in/{first} -o {output_dir}/{first} --preset-import-file {input_dir}/{handbrake_preset}.json -Z "{handbrake_preset}"'
         os.system(st)
-        print(st)
-        keyboard.press('enter')
         processing.pop()
-        os.system("rm {0}/in/{1}".format(inp,first))
+        os.system(f'rm {input_dir}/in/{first}')
         try:
-            os.system('scp "{0}/{2}" "{3}:{1}"'.format(output,remote,first, adress))
+            os.system(f'scp "{output_dir}/{first}" "{scp_adress}:{remote_dir}"')
         except:
             print("Client not available")
 
